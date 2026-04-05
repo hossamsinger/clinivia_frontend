107a534202ca0e65d38c9e8e0e8865d52edce145
Hussam Mustafa <hossam.moustaf.777@gmail.com>
2026-04-05 12:40:18 +0200

    version 2 dashboard

--- src/app/features/dashboards/patient/patient.component.ts
+++ src/app/features/dashboards/patient/patient.component.ts
@@ -1,15 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-patient',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './patient.component.html',
   styleUrl: './patient.component.scss',
 })
 export class PatientDashboardComponent implements OnInit {
-
-   fullText = ' أ / حسام مصطفي ';
+  fullText = ' أ / حسام مصطفي ';
   displayedText = '';
   index = 0;
 
@@ -30,4 +30,34 @@ export class PatientDashboardComponent implements OnInit {
       }, 2000);
     }
   }
+  
+  cards = [
+    {
+      title: 'ضغط الدم',
+      value: '١١٠/٧٠',
+      img: 'assets/images/blood-pressure.png',
+      trend: ' ١٠٪ أعلى من الشهر الماضي',
+    },
+    {
+      title: 'معدل ضربات القلب',
+      value: '٦٥٠',
+      img: 'assets/images/heart-rate.png',
+      trend: ' ٧٪ أقل من الشهر الماضي',
+    },
+    {
+      title: 'مستوى السكر',
+      value: '٨٨ - ٧٥',
+      img: 'assets/images/glucose.png',
+      trend: ' ١٢٪ أعلى من الشهر الماضي',
+    },
+    {
+      title: 'عدد خلايا الدم',
+      value: '٩٬٤٥٦/مل',
+      img: 'assets/images/blood-count.png',
+      trend: ' ٢٢٪ أقل من الشهر الماضي',
+    },
+  ];
+
+
+  
 }
